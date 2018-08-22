99e5624ef64698cf8121dfb8160e8c2158ded672
ninedraft <clamores.pro@gmail.com>
2018-08-22 17:25:46 +0300

    add todo

--- embark/cmd/embark/main.go
+++ embark/cmd/embark/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/containerum/containerum/embark/pkg/builder"
+)
+
+func main() {
+	var client = builder.NewCLient(":1245")
+	fmt.Println(client.Install())
 }
---
+++ embark/todo.md
@@ -0,0 +1,4 @@
+TODO:
+
+- [x] import installer from helm client
+- [ ] make test
