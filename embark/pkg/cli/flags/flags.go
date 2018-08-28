04c8bc52ec3515f242d3c9ab3a1eba743f9390a9
ninedraft <clamores.pro@gmail.com>
2018-08-28 16:58:27 +0300

    refactor add model

---
+++ embark/pkg/cli/flags/flags.go
@@ -0,0 +1,9 @@
+package flags
+
+type Install struct {
+	KubeConfig string `json:"kube_config"`
+	Namespace  string `json:"namespace"`
+	Host       string `json:"host"`
+	Dir        string `json:"dir"`
+	Values     string `json:"values"`
+}
