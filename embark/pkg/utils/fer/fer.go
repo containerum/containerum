dc8c43bcc875d154c149529c5c7a71f1396a9917
ninedraft <clamores.pro@gmail.com>
2018-09-04 15:26:14 +0300

    add formatted stderr printer

---
+++ embark/pkg/utils/fer/fer.go
@@ -0,0 +1,34 @@
+package fer
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+var (
+	mu sync.Mutex
+)
+
+func Println(args ...interface{}) (int, error) {
+	mu.Lock()
+	defer mu.Unlock()
+	return fmt.Fprintln(os.Stderr, args...)
+}
+
+func Printf(ff string, args ...interface{}) (int, error) {
+	mu.Lock()
+	defer mu.Unlock()
+	return fmt.Fprintf(os.Stdout, ff, args...)
+}
+
+func Print(args ...interface{}) (int, error) {
+	mu.Lock()
+	defer mu.Unlock()
+	return fmt.Fprint(os.Stdout, args...)
+}
+
+func Fatal(ff string, args ...interface{}) {
+	Printf(ff, args...)
+	os.Exit(1)
+}
