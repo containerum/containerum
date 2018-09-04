92e7ba7eeb011a11c138173eb42d2a96a9560ec0
ninedraft <clamores.pro@gmail.com>
2018-09-04 11:35:43 +0300

    add installation ordering

--- embark/pkg/builder/install.go
+++ embark/pkg/builder/install.go
@@ -2,13 +2,26 @@ package builder
 
 import (
 	"fmt"
-	"path"
-
 	"io/ioutil"
+	"path"
 
 	"github.com/containerum/containerum/embark/pkg/cgraph"
 	"github.com/containerum/containerum/embark/pkg/emberr"
+	kubeCoreV1 "k8s.io/api/core/v1"
 	"k8s.io/helm/pkg/helm"
+)
+
+var (
+	_ = kubeCoreV1.PersistentVolumeClaim{}
+)
+
+const (
+	Deployment = "deployment"
+	Ingress    = "ingress"
+	Service    = "service"
+	Volume     = "volume"
+	Job        = "job"
+	Configmap  = "configmap"
 )
 
 func (client *Client) InstallChartWithDependencies(namespace, dir string, valuesFile string) error {
@@ -66,7 +79,6 @@ func (client *Client) InstallChartWithDependencies(namespace, dir string, values
 	return nil
 }
 
-// NOTE:
 func (client *Client) Install(namespace, dir string) error {
 	var ch, loadChartErr = client.LoadChartFromDir(dir)
 	if loadChartErr != nil {
@@ -77,35 +89,80 @@ func (client *Client) Install(namespace, dir string) error {
 		return err
 	}
 
-	for _, depl := range rendered.Deployments {
-		var _, createDeplErr = client.
-			kube.
-			AppsV1().
-			Deployments(namespace).
-			Create(&depl)
-		if createDeplErr != nil {
-			return createDeplErr
-		}
-	}
+	var installationOrder = []string{Volume, Configmap, Job, Deployment, Service, Ingress}
 
-	for _, serv := range rendered.Services {
-		var _, createServErr = client.
-			kube.
-			CoreV1().
-			Services(namespace).
-			Create(&serv)
-		if createServErr != nil {
-			return err
-		}
-	}
-
-	for _, ingr := range rendered.Ingresses {
-		var _, createIngr = client.
-			kube.ExtensionsV1beta1().
-			Ingresses(namespace).
-			Create(&ingr)
-		if createIngr != nil {
-			return createIngr
+	for _, resourceType := range installationOrder {
+		switch resourceType {
+		case Volume:
+			// TODO: add
+			/*
+				for _, vol := range rendered.Volumes {
+					_ = vol
+					var _, createVolumeErr = client.
+						kube.CoreV1().
+						PersistentVolumeClaims("namespace").
+						Create(vol.PersistentVolumeClaim)
+					if createVolumeErr != nil {
+						return createVolumeErr
+					}
+				}
+			*/
+		case Configmap:
+			for _, configmap := range rendered.Configs {
+				var _, createConfigmapErr = client.
+					kube.
+					CoreV1().
+					ConfigMaps(namespace).
+					Create(&configmap)
+				if createConfigmapErr != nil {
+					return createConfigmapErr
+				}
+			}
+		case Job:
+			for _, job := range rendered.Jobs {
+				var _, createJobErr = client.
+					kube.
+					BatchV1().
+					Jobs(namespace).
+					Create(&job)
+				if createJobErr != nil {
+					return createJobErr
+				}
+			}
+		case Deployment:
+			for _, depl := range rendered.Deployments {
+				var _, createDeplErr = client.
+					kube.
+					AppsV1().
+					Deployments(namespace).
+					Create(&depl)
+				if createDeplErr != nil {
+					return createDeplErr
+				}
+			}
+		case Service:
+			for _, serv := range rendered.Services {
+				var _, createServErr = client.
+					kube.
+					CoreV1().
+					Services(namespace).
+					Create(&serv)
+				if createServErr != nil {
+					return err
+				}
+			}
+		case Ingress:
+			for _, ingr := range rendered.Ingresses {
+				var _, createIngrErr = client.
+					kube.ExtensionsV1beta1().
+					Ingresses(namespace).
+					Create(&ingr)
+				if createIngrErr != nil {
+					return createIngrErr
+				}
+			}
+		default:
+			panic(fmt.Sprintf("[embark.pkg.builder.Client.Install] unexected installation order state: %q", resourceType))
 		}
 	}
 
