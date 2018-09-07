633beb55ec8b7b6e21be34b215c55d039c806b87
ninedraft <clamores.pro@gmail.com>
2018-09-07 17:22:41 +0300

    use object factory

--- embark/pkg/kube/config.go
+++ embark/pkg/kube/config.go
@@ -1,12 +1,8 @@
 package kube
 
 import (
-	"io/ioutil"
-	"os"
-
 	"time"
 
-	"gopkg.in/yaml.v2"
 	kubeClientAPI "k8s.io/client-go/tools/clientcmd/api"
 )
 
@@ -29,39 +25,4 @@ func (config Config) Getter() func() (*kubeClientAPI.Config, error) {
 	return func() (*kubeClientAPI.Config, error) {
 		return config.ToKube(), nil
 	}
-}
-
-type ConfigProvider func() (Config, error)
-
-var (
-	_ ConfigProvider = AutoConfig
-)
-
-// Looks for the configuration file in the "~/.kube/config", if it is defined by KUBECONFIG env, then it tries to load from there.
-func AutoConfig() (Config, error) {
-	var configPath = "~/.kube/config"
-	{
-		var envConfigPath, envConfigPathDefined = os.LookupEnv("KUBECONFIG")
-		if envConfigPathDefined {
-			configPath = envConfigPath
-		}
-	}
-	return LoadConfigFromFile(configPath)
-}
-
-// Creates config provider, which will load data from provided file
-func FileConfigProvider(filename string) ConfigProvider {
-	return func() (Config, error) {
-		return LoadConfigFromFile(filename)
-	}
-}
-
-// Loads kube config from provided file
-func LoadConfigFromFile(filename string) (Config, error) {
-	var data, readFileErr = ioutil.ReadFile(filename)
-	if readFileErr != nil {
-		return Config{}, readFileErr
-	}
-	var config Config
-	return config, yaml.Unmarshal(data, &config)
 }
--- embark/pkg/kube/kube.go
+++ embark/pkg/kube/kube.go
@@ -1,6 +1,7 @@
 package kube
 
 import (
+	"io"
 	"time"
 
 	"github.com/containerum/containerum/embark/pkg/emberr"
@@ -8,12 +9,16 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	kubeClientAPI "k8s.io/client-go/tools/clientcmd/api"
+	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/resource"
 )
 
 type Kube struct {
 	Config kubeClientAPI.Config
 	*kubernetes.Clientset
-	config *rest.Config
+	config  *rest.Config
+	factory cmdutil.Factory
 }
 
 func NewKubeClient(config Config) (*Kube, error) {
@@ -30,7 +35,46 @@ func NewKubeClient(config Config) (*Kube, error) {
 	if newClientsetErr != nil {
 		return nil, emberr.ErrUnableToCreateKubeCLient{Reason: newClientsetErr, Comment: "while creating clientset from REST config"}
 	}
+	kube.factory = cmdutil.NewFactory(configFlags())
 	kube.Clientset = kubecli
 	kube.config = restConfig
 	return &kube, nil
+}
+
+func configFlags() *genericclioptions.ConfigFlags {
+	var config = genericclioptions.NewConfigFlags()
+	return config
+}
+
+func (kube *Kube) builder(namespace string, reader io.Reader) *resource.Result {
+	return kube.factory.NewBuilder().
+		ContinueOnError().
+		NamespaceParam(namespace).
+		Stream(reader, "").
+		DefaultNamespace().
+		Flatten().
+		Do()
+}
+
+func (kube *Kube) InstallFromReader(namespace string, re io.Reader) error {
+	var infos, builderErr = kube.builder(namespace, re).Infos()
+	if builderErr != nil {
+		return builderErr
+	}
+	for _, info := range infos {
+		kube.RESTClient().
+			Post().
+			Body(info.Object).
+			Do()
+		var obj, createErr = resource.
+			NewHelper(kube.RESTClient(), info.Mapping).
+			Create(namespace, true, info.Object)
+		if createErr != nil {
+			return createErr
+		}
+		if err := info.Refresh(obj, false); err != nil {
+			return err
+		}
+	}
+	return nil
 }
