b5a133b34ede637a628b8ebb377c9183c903a30d
ninedraft <clamores.pro@gmail.com>
2018-09-10 17:27:14 +0300

    add docs

--- embark/pkg/ogetter/retrieve.go
+++ embark/pkg/ogetter/retrieve.go
@@ -2,6 +2,7 @@ package ogetter
 
 import "bytes"
 
+// Loads content of selected objects to map[string]*bytes.Buffer
 func RetrieveObjects(getter ObjectGetter, objectNames ...string) (map[string]*bytes.Buffer, error) {
 	var objects = make(map[string]*bytes.Buffer, len(objectNames))
 	for _, objectName := range objectNames {
