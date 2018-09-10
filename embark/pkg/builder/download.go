5e17101bf4fccfb310c78674232608c083b01166
ninedraft <clamores.pro@gmail.com>
2018-09-10 18:26:33 +0300

    rebuild download func

--- embark/pkg/builder/download.go
+++ embark/pkg/builder/download.go
@@ -1,47 +1,18 @@
 package builder
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/containerum/containerum/embark/pkg/emberr"
-	"github.com/containerum/containerum/embark/pkg/models/requirements"
 	"github.com/mholt/archiver"
 )
 
-func (client *Client) downloadDependency(getter *http.Client, dir string, chart requirements.Dependency) error {
-
-	client.downloadMu.Lock() // Lock
-
-	var resp, err = func() (*http.Response, error) {
-		fmt.Printf("Downloading and unpacking %q...", func() string {
-			var u, _ = chart.URL()
-			return u
-		}())
-
-		defer client.downloadMu.Unlock() // Unlock
-
-		var addr, err = chart.URL()
-		if err != nil {
-			return nil, emberr.ErrUnableToDownloadDependencies{Reason: err}
-		}
-		var resp, errGet = getter.Get(addr)
-		if errGet != nil {
-			return nil, emberr.ErrUnableToDownloadDependencies{Reason: errGet}
-		}
-		var size = resp.ContentLength
-		if size <= 0 {
-			size = 2 * 1 << 10
-		}
-		fmt.Printf("%d bytes\n", size)
-		return resp, nil
-	}()
+func downloadDependency(getter *http.Client, dir, componentURI string) error {
+	var resp, err = getter.Get(componentURI)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	// var bar = pb.New(int(resp.ContentLength))
-	// bar.Start()
 	var errUnpack = archiver.TarGz.Read(resp.Body, dir)
 	// bar.Finish()
 	if errUnpack != nil {
