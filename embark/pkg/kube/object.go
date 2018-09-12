b5d6f1144cfcd6cf7d7f22882dbd52b85ecbcec2
ninedraft <clamores.pro@gmail.com>
2018-09-12 14:16:15 +0300

    add generic kube object model

---
+++ embark/pkg/kube/object.go
@@ -0,0 +1,106 @@
+package kube
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/ericchiang/k8s"
+	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
+	"github.com/mitchellh/mapstructure"
+	"github.com/thoas/go-funk"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	_ json.Marshaler   = Object{}
+	_ json.Unmarshaler = new(Object)
+
+	_ yaml.Marshaler   = Object{}
+	_ yaml.Unmarshaler = new(Object)
+
+	_ k8s.Resource = Object{}
+)
+
+func init() {
+	k8s.Register("embark", "v1", "embark-generic-object", true, new(Object))
+}
+
+type Object struct {
+	meta *metav1.ObjectMeta
+	body map[string]interface{}
+}
+
+func (object Object) Query(path string) interface{} {
+	return funk.Get(object.body, path)
+}
+
+func (object Object) String() string {
+	var data, err = yaml.Marshal(object)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+func (object Object) ToStruct(target interface{}) error {
+	return mapstructure.WeakDecode(object.body, target)
+}
+
+func ObjectFromJSON(re io.Reader) (Object, error) {
+	var object = Object{
+		body: make(map[string]interface{}),
+	}
+	return object, json.NewDecoder(re).Decode(object.body)
+}
+
+func ObjectFromYAML(re io.Reader) (Object, error) {
+	var object = Object{
+		body: make(map[string]interface{}),
+	}
+	return object, yaml.NewDecoder(re).Decode(object.body)
+}
+
+func (object *Object) PatchMeta(patchers ...func(meta *metav1.ObjectMeta)) {
+	object.initMeta()
+	for _, patcher := range patchers {
+		patcher(object.meta)
+	}
+}
+
+func ObjectFromStruct(data interface{}) (Object, error) {
+	var object Object
+	return object, mapstructure.Decode(data, &object.body)
+}
+
+func (object *Object) initMeta() *metav1.ObjectMeta {
+	if object.meta == nil {
+		object.meta = new(metav1.ObjectMeta)
+	}
+	if err := mapstructure.WeakDecode(object.body["metadata"], object.meta); err != nil {
+		panic(err)
+	}
+	return object.meta
+}
+
+func (object Object) GetMetadata() *metav1.ObjectMeta {
+	if object.meta == nil {
+		object.initMeta()
+	}
+	return object.meta
+}
+
+func (object Object) MarshalJSON() ([]byte, error) {
+	return json.Marshal(object.body)
+}
+
+func (object *Object) UnmarshalJSON(data []byte) error {
+	return yaml.Unmarshal(data, &object.body)
+}
+
+func (object Object) MarshalYAML() (interface{}, error) {
+	return object.body, nil
+}
+
+func (object *Object) UnmarshalYAML(unmarshaler func(interface{}) error) error {
+	return unmarshaler(&object.body)
+}
