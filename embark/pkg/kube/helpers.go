601e473834a59d363c981510dc66b88d8606a8ba
ninedraft <clamores.pro@gmail.com>
2018-09-13 17:34:53 +0300

    use kubeconf decoders

--- embark/pkg/kube/helpers.go
+++ embark/pkg/kube/helpers.go
@@ -1,18 +1,10 @@
 package kube
 
 import (
-	"bytes"
-	"encoding/base64"
-	"fmt"
 	"io"
 	"os"
 
-	"github.com/thoas/go-funk"
-
-	"github.com/mitchellh/mapstructure"
-
-	"github.com/containerum/containerum/embark/pkg/utils/ym"
-	"github.com/ghodss/yaml"
+	"github.com/containerum/containerum/embark/pkg/utils/kubeconf"
 )
 
 var (
@@ -20,50 +12,28 @@ var (
 )
 
 func StdKubectConfig() (KubectlConfig, error) {
-	var config KubectlConfig
-	return config, ym.LoadYAML(autoKubectlConfigPath(), &config)
+	var config, err = kubeconf.LoadFromFile(autoKubectlConfigPath())
+	return KubectlConfig(config), err
 }
 
 func LoadKubectlConfigFromPath(configPath string) KubectlConfigProvider {
 	return func() (KubectlConfig, error) {
-		var config KubectlConfig
-		return config, ym.LoadYAML(configPath, &config)
+		var config, err = kubeconf.LoadFromFile(configPath)
+		if err != nil {
+			return KubectlConfig{}, err
+		}
+		return KubectlConfig(config), err
 	}
 }
 
 func KubectlConfigFromReader(re io.Reader) KubectlConfigProvider {
-	var config KubectlConfig
-	var buf = &bytes.Buffer{}
-	if _, err := buf.ReadFrom(re); err != nil {
-		return config.AsProviderWithErr(err)
-	}
-	return config.AsProviderWithErr(yaml.Unmarshal(buf.Bytes(), &config))
+	var config, err = kubeconf.LoadFromReader(re)
+	return KubectlConfig(config).AsProviderWithErr(err)
 }
 
 func DecodeConfig(data []byte) (KubectlConfig, error) {
-	var tree map[string]interface{}
-	if err := yaml.Unmarshal(data, &tree); err != nil {
-		return KubectlConfig{}, err
-	}
-	var config KubectlConfig
-	var meta = &mapstructure.Metadata{}
-	if err := mapstructure.WeakDecodeMetadata(tree, &config, meta); err != nil {
-		return KubectlConfig{}, err
-	}
-	switch cert := funk.Get(tree, "clusters").(type) {
-	case nil:
-		// pass
-	case string:
-		var certData, err = base64.StdEncoding.DecodeString(cert)
-		if err != nil {
-			return KubectlConfig{}, err
-		}
-		config.Clusters[0].Cluster.CertificateAuthorityData = certData
-	default:
-		fmt.Printf("%T\n%v", cert, cert)
-	}
-
-	return config, nil
+	var config, err = kubeconf.Load(data)
+	return KubectlConfig(config), err
 }
 
 func autoKubectlConfigPath() string {
