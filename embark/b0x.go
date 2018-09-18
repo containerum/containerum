15f1d300748baab971c8595ff96c43f3f331a07f
ninedraft <clamores.pro@gmail.com>
2018-09-18 12:56:48 +0300

    embed all resources from main chart dir

--- embark/b0x.go
+++ embark/b0x.go
@@ -1,24 +1,3 @@
-package main
-
-import (
-	"os"
-	"os/exec"
-
-	"github.com/containerum/containerum/embark/pkg/utils/fer"
-)
+package embark
 
 //go:generate fileb0x b0x.toml
-
-func main() {
-	var fileb0x = exec.Command("fileb0x", "b0x.toml")
-	fileb0x.Stdout = os.Stdout
-	fileb0x.Stderr = os.Stderr
-	var getWDErr error
-	fileb0x.Dir, getWDErr = os.Getwd()
-	if getWDErr != nil {
-		panic(getWDErr)
-	}
-	if err := fileb0x.Run(); err != nil {
-		fer.Fatal("unable to generate static embedded filesystem:\n%v\n", err)
-	}
-}
