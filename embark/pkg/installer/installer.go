8fc652d7a2f2f70667863dd162bc07a1f5dff537
ninedraft <clamores.pro@gmail.com>
2018-09-21 15:15:31 +0300

    add cached charts (aka subcharts) support

--- embark/pkg/installer/installer.go
+++ embark/pkg/installer/installer.go
@@ -3,12 +3,18 @@ package installer
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
+	"time"
 
 	"github.com/containerum/containerum/embark/pkg/builder"
+	"github.com/containerum/containerum/embark/pkg/depsearch"
 	"github.com/containerum/containerum/embark/pkg/emberr"
 	"github.com/containerum/containerum/embark/pkg/kube"
+	"github.com/containerum/containerum/embark/pkg/models/components"
+	"github.com/containerum/containerum/embark/pkg/render"
+	"github.com/containerum/containerum/embark/pkg/utils/why"
 )
 
 type Installer struct {
@@ -27,15 +33,46 @@ func (installer Installer) Install() error {
 			Reason: err,
 		}
 	}
-	var containerumConfig, loadContDataErr = loadContainerumConfig(installer.ContainerumConfigPath)
+	var containerumComponents, loadContDataErr = loadContainerumConfig(installer.ContainerumConfigPath)
 	if loadContDataErr != nil {
 		return loadContDataErr
 	}
-	if err := builder.DowloadComponents(installer.TempDir, containerumConfig); err != nil {
-		return err
+
+	if containerumComponents.Contains(render.Containerum) {
+		var getter = &http.Client{
+			Timeout: 10 * time.Second,
+		}
+		var downloadContainerumErr = builder.DownloadComponent(getter,
+			installer.TempDir,
+			containerumComponents.MustGet(render.Containerum).URL())
+		if downloadContainerumErr != nil {
+			return downloadContainerumErr
+		}
 	}
 
-	var rendered, renderErr = builder.RenderComponents(installer.TempDir, containerumConfig, builder.RenderWithValues(map[string]interface{}{}))
+	var chartIndex, buildingChartIndexErr = depsearch.NewSearcher(installer.TempDir)
+	if buildingChartIndexErr != nil {
+		return buildingChartIndexErr
+	}
+
+	var notDownloadedComponents = containerumComponents.
+		Filter(func(component components.ComponentWithName) bool {
+			return !chartIndex.Contains(component.Name)
+		})
+
+	if notDownloadedComponents.Len() > 0 {
+		why.Print("Components to download", notDownloadedComponents.Names()...)
+		if err := builder.DownloadComponents(installer.TempDir, notDownloadedComponents); err != nil {
+			return err
+		}
+		// ! rebuild index!
+		chartIndex, buildingChartIndexErr = depsearch.NewSearcher(installer.TempDir)
+		if buildingChartIndexErr != nil {
+			return buildingChartIndexErr
+		}
+	}
+
+	var rendered, renderErr = builder.RenderComponents(installer.TempDir, containerumComponents, builder.RenderWithValues(map[string]interface{}{}))
 	if renderErr != nil {
 		return renderErr
 	}
