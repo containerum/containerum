3d413e9c16560e394fe91b7089a9cb38fd43e036
ninedraft <clamores.pro@gmail.com>
2018-09-26 13:47:47 +0300

    fix test TestRenderer

--- embark/pkg/renderer/renderer_test.go
+++ embark/pkg/renderer/renderer_test.go
@@ -2,9 +2,14 @@ package renderer
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/containerum/containerum/embark/pkg/models/chart"
 
 	"github.com/containerum/containerum/embark/pkg/kube"
 	"github.com/containerum/containerum/embark/pkg/ogetter"
@@ -14,7 +19,7 @@ import (
 )
 
 func TestRenderer(test *testing.T) {
-	var getter = ogetter.NewFSObjectGetter("testdata/postgresql")
+	var getter = ogetter.NewFSObjectGetter("testdata/postgresql/templates")
 	test.Log(getter.ObjectNames())
 
 	var component, rendererErr = Renderer{
@@ -30,7 +35,18 @@ func TestRenderer(test *testing.T) {
 	if rendererErr != nil {
 		test.Fatal(rendererErr)
 	}
-	test.Log(component.Objects())
+	var objects = component.Objects()
+	//test.Log(objects)
+	for i, object := range objects {
+		var mock, ok = object.(mockObject)
+		if !ok {
+			test.Logf("object %d has invalid type %t", i, object)
+		}
+		var t interface{}
+		if err := yaml.Unmarshal(mock.Bytes(), &t); err != nil {
+			test.Fatalf("%v\n\n%s", err, lines(mock.String()))
+		}
+	}
 }
 
 var (
@@ -50,9 +66,37 @@ func (mockObject) GetMetadata() *v1.ObjectMeta {
 }
 
 func testValues(test *testing.T) map[string]interface{} {
-	var values map[string]interface{}
-	var data, loadTestValuesErr = ioutil.ReadFile("testdata/postgresql/values.yaml")
-	assert.Nil(test, loadTestValuesErr)
-	assert.Nil(test, yaml.Unmarshal(data, &values))
-	return values
+	var chartValues map[string]interface{}
+	{
+		var data, loadTestValuesErr = ioutil.ReadFile("testdata/postgresql/values.yaml")
+		assert.Nil(test, loadTestValuesErr)
+		assert.Nil(test, yaml.Unmarshal(data, &chartValues))
+	}
+	var ch chart.Chart
+	{
+		var data, loadTestValuesErr = ioutil.ReadFile("testdata/postgresql/Chart.yaml")
+		assert.Nil(test, loadTestValuesErr)
+		assert.Nil(test, yaml.Unmarshal(data, &ch))
+	}
+	chartValues["strategy"] = "restart-always"
+	return map[string]interface{}{
+		"Values": chartValues,
+		"Chart":  ch,
+		"Release": map[string]interface{}{
+			"Name":    "containerum",
+			"Service": "containerum",
+		},
+	}
+}
+
+func lines(text string) string {
+	var lines = strings.Split(text, "\n")
+	var maxLineNumberTextLen = len(strconv.Itoa(len(lines)))
+	var lineNumberAligment = "%" + strconv.Itoa(maxLineNumberTextLen) + "d"
+	fmt.Printf("%s\n", lineNumberAligment)
+	var buf = &bytes.Buffer{}
+	for i, line := range lines {
+		fmt.Fprintf(buf, lineNumberAligment+" %s\n", i+1, line)
+	}
+	return buf.String()
 }
