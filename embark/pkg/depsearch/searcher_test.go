4a3ebc8057646329d42d0a935b3339201b8fb26c
ninedraft <clamores.pro@gmail.com>
2018-09-28 11:54:37 +0300

    change component to search

--- embark/pkg/depsearch/searcher_test.go
+++ embark/pkg/depsearch/searcher_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestStatic(test *testing.T) {
 	var searcher = Static()
-	for _, componentName := range []string{"postgresql"} {
+	for _, componentName := range []string{"auth"} {
 		if !searcher.Contains(componentName) {
 			test.Fatalf("unable to find component %q in static FS", componentName)
 		}
