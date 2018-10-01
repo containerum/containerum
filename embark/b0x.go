64d2965cf07d86e421ee44a8794d52bdb926e879
ninedraft <clamores.pro@gmail.com>
2018-10-01 16:54:44 +0300

    use go bindata #61

--- embark/b0x.go
+++ embark/b0x.go
@@ -1,4 +1,6 @@
 package embark
 
 //go:generate go build -v -o ./bin/fileb0x ./vendor/github.com/UnnoTed/fileb0x
-//go:generate ./bin/fileb0x b0x.toml
+// - go:generate go build -v -o ./bin/packr ./vendor/github.com/gobuffalo/packr
+// - go:generate go build -v -o ./bin/go-bindata-assetfs ./vendor/github.com/elazarl/go-bindata-assetfs
+//go:generate ./bin/bindata b0x.toml
